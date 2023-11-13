c03fb3855b7c20d450fe2b6ef9b30a31422d8e66
Carlos Loitti <carlosloitir@gmail.com>
2023-11-13 10:17:44 -0500

    Index Products

---
+++ Models/PayMode.cs
@@ -0,0 +1,11 @@
+﻿namespace SupermarketWEB2.Models
+{
+    public class PayMode
+    {
+        public int Pay_Mode_Id { get; set; }
+        public string Pay_Mode_Name { get; set; }
+        public string? Pay_Mode_Observation { get; set; }
+
+        public ICollection<Product>? Products { get; set; } = default!;
+    }
+}
