7c61b1870a3f3d110247d0ed4490f64e0d13bbaf
Carlos Loitti <carlosloitir@gmail.com>
2023-11-16 17:47:37 -0500

    Model User - Razor Page Login

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketWEB2.Models
+{
+    public class User
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+
+        public string Password { get; set; }
+    }
+}
