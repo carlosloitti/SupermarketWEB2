d646be5cf94a357f468f4b2737b09de1494917dd
Carlos Loitti <carlosloitir@gmail.com>
2023-11-16 17:27:22 -0500

    Index Providers

---
+++ Pages/Providers/Index.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB2.Data;
+using SupermarketWEB2.Models;
+
+namespace SupermarketWEB2.Pages.Providers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public IndexModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Provider> Providers { get; set; } = default!;
+
+
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Providers != null)
+            {
+                Providers = await _context.Providers.ToListAsync();
+            }
+
+        }
+    }
+}
