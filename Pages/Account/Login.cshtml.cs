cac7ed8b1ef157728dc27faa1c2bcb99a80de3be
Carlos Loitti <carlosloitir@gmail.com>
2023-11-16 18:22:47 -0500

    Codigo Login.cshtml - Metodo OnPost

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB2.Models;
 
 namespace SupermarketWEB2.Pages.Account
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        public User User { get; set; }
+
+
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult>OnPostAsync () {
+
+            if (!ModelState.IsValid) return Page();
+
+            if (User.Email == "correo@gmail.com" && User.Password == "12345" )  
+            {
+                return RedirectToPage("/index");
+            }
+            return Page();
         }
     }
 }
